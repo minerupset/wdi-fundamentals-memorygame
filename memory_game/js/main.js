9d2c21f8f45eb91ae6224d9bda5a25ab8ccbaa3e
minerupset <35759470+minerupset@users.noreply.github.com>
2020-03-09 23:09:06 -0700

    Added very rudimentary structure for the card game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log(`User flipped ${cardsInPlay[0]}`);
+console.log(`User flipped ${cardsInPlay[1]}`);
+
+if (cardsInPlay.length === 2) {
+  cardsInPlay[0] === cardsInPlay[1]
+    ? alert("You found a match!")
+    : alert("Sorry, try again");
+}
